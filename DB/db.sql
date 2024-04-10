bf044c71df313f2b1cedd1fddb90eccac08a3889
iskandar <silvapaco810@gmail.com>
2024-04-10 16:03:23 -0600

    db

--- DB/db.sql
+++ DB/db.sql
@@ -4,7 +4,7 @@ CREATE TABLE memory_requests (
     id INT AUTO_INCREMENT PRIMARY KEY,
     pid INT,
     process_name VARCHAR(255),
-    call VARCHAR(50),
+    call_ VARCHAR(10),
     memory_size INT,
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );

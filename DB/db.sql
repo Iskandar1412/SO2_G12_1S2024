9335a6de222c762a78bea7860e6d446ad9f33ea9
Paco Silva <silvapaco810@gmail.com>
2024-04-12 20:38:42 -0600

    Systemtap completo

--- DB/db.sql
+++ DB/db.sql
@@ -5,7 +5,7 @@ CREATE TABLE SOPES (
     id INT AUTO_INCREMENT PRIMARY KEY,
     pid INT,
     process_name VARCHAR(255),
-    call_type ENUM('mmap', 'munmap'),
+    call_type VARCHAR(20),
     memory_size INT,
     request_datetime DATETIME
 );
--- VM/lector.c
+++ VM/lector.c
@@ -2,66 +2,77 @@
 #include <stdlib.h>
 #include <string.h>
 #include <mysql/mysql.h>
+#include <time.h>
 
 // sudo apt-get install libmysqlclient-dev
 // gcc lector.c -o lector -I/usr/include/mysql -lmysqlclient
 // gcc lector.c -o lector -lmysqlclient
-// sudo stap -g systemtap.stp | ./lector
+// sudo stap systemtap.stp | ./lector
 
 #define SERVER "172.17.0.2"
 #define USER "root"
 #define PASSWORD "iskandar"
 #define DATABASE "SOPES"
 
-void insert_to_database(MYSQL *conn, int pid, char *process_name, char *call_type, int memory_size) {
-    char query[512];
-    sprintf(query, "INSERT INTO SOPES (pid, process_name, call_type, memory_size, request_datetime) VALUES (%d, '%s', '%s', %d, NOW())", pid, process_name, call_type, memory_size);
+#define MAX_QUERY_LEN 500
 
-    if (mysql_query(conn, query)) {
-        fprintf(stderr, "%s\n", mysql_error(conn));
-        mysql_close(conn);
-        exit(1);
-    }
+MYSQL *conn;
+
+void finish_with_error()
+{
+    fprintf(stderr, "%s\n", mysql_error(conn));
+    mysql_close(conn);
+    exit(1);
 }
 
-int main() {
-    MYSQL *conn = mysql_init(NULL);
-    if (conn == NULL) {
-        fprintf(stderr, "Error initializing MySQL: %s\n", mysql_error(conn));
+int main()
+{
+    MYSQL_RES *res;
+    MYSQL_ROW row;
+    char query[MAX_QUERY_LEN];
+
+    conn = mysql_init(NULL);
+    if (conn == NULL)
+    {
+        fprintf(stderr, "mysql_init() failed\n");
         exit(1);
     }
 
-    if (mysql_real_connect(conn, SERVER, USER, PASSWORD, DATABASE, 0, NULL, 0) == NULL) {
-        fprintf(stderr, "Error connecting to MySQL: %s\n", mysql_error(conn));
+    if (mysql_real_connect(conn, SERVER, USER, PASSWORD, DATABASE, 0, NULL, 0) == NULL)
+    {
+        fprintf(stderr, "Error connecting to database: %s\n", mysql_error(conn));
         mysql_close(conn);
         exit(1);
     }
-    printf("Conectado\n");
 
-    
-    // Ejecutar STP
-    FILE *stap_output = popen("sudo stap -g systemtap.stp", "r");
-    if (stap_output == NULL) {
-        fprintf(stderr, "Error executing SystemTap\n");
+    printf("Connected to MySQL database successfully\n");
+
+    // Your SystemTap script goes here
+    // For simplicity, I'll just simulate the data insertion
+    // Replace this with your actual data retrieval logic from SystemTap
+
+    int pid = 1234; // Example PID
+    char process_name[] = "example_process";
+    char call_type[] = "mmap";
+    int memory_size = 1024; // Example memory size
+    char request_datetime[] = "2024-04-12 12:00:00"; // Example datetime
+
+    // Prepare the SQL statement
+    sprintf(query, "INSERT INTO SOPES (pid, process_name, call_type, memory_size, request_datetime) VALUES (%d, '%s', '%s', %d, '%s')",
+            pid, process_name, call_type, memory_size, request_datetime);
+
+    // Execute the SQL statement
+    if (mysql_query(conn, query))
+    {
+        fprintf(stderr, "Query execution failed: %s\n", mysql_error(conn));
         mysql_close(conn);
         exit(1);
     }
 
-    char line[512];
-    while (fgets(line, sizeof(line), stap_output)) {
-        printf("%s", line);
-        int pid, memory_size;
-        char process_name[256], call_type[16];
-
-        sscanf(line, "%d,%255[^,],%15[^,],%d", &pid, process_name, call_type, &memory_size);
-        insert_to_database(conn, pid, process_name, call_type, memory_size);
-    }
-
-    // Cerrar DB
-    pclose(stap_output);
-    printf("End\n");
+    printf("Data inserted successfully\n");
 
     mysql_close(conn);
+
     return 0;
 }
 
@@ -72,4 +83,8 @@ sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 100 --slave 
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 90 --slave /usr/bin/g++ g++ /usr/bin/g++-12 --slave /usr/bin/gcov gcov /usr/bin/gcov-12
 
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 80 --slave /usr/bin/g++ g++ /usr/bin/g++-10 --slave /usr/bin/gcov gcov /usr/bin/gcov-10
+*/
+
+/*
+
 */
